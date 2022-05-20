25af5ded5f24658fb1bc8a53c2c834df04cad351
ReidBurger <rb1504@messiah.edu>
2022-05-20 11:39:38 -0400

    End Day 11

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float speed = 5;
+    public float leftBound = -10;
+    public float rightBound = 10;
+    public float upBound = 10;
+    public float downBound = -10;
+    [SerializeField]
+    private GameObject laserPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerMovement();
+        outOfBounds();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(laserPrefab, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+        }
+    }
+
+    void playerMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * speed * Time.deltaTime);
+    }
+
+    void outOfBounds()
+    {
+        if (transform.position.x > leftBound && transform.position.x < rightBound && transform.position.y > downBound && transform.position.y < upBound)
+        {
+            return;
+        }
+        else
+        {
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, leftBound, rightBound), transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, downBound, upBound), 0);
+        }
+    }
+}
